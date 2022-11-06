ec7df9f2080602f799ed99032f8495c9d9f3a5d4
guonaihong <guonaihong@qq.com>
2022-11-06 22:33:42 +0800

    更新测试脚本

--- testscript.sh
+++ testscript.sh
@@ -1,8 +1,77 @@
 #!/bin/bash
 
 
-###### 第一个测试是只控制数量, 观察cpu, memory
-# 运行10个任务
-# 运行100个任务
-# 运行1000个任务
-# 运行10000个任务
+function cron_10() {
+	# 运行10个任务
+	cron_inner "cronex" 10
+	cron_inner "cron" 10
+}
+
+function cron_100() {
+	# 运行100个任务
+	cron_inner "cronex" 100
+	cron_inner "cron" 100
+}
+
+function cron_1000() {
+	# 运行1000个任务
+	cron_inner "cronex" 1000
+	cron_inner "cron" 1000
+}
+
+function cron_10000() {
+	# 运行10000个任务
+	cron_inner "cronex" 1000
+	cron_inner "cron" 10000
+}
+
+function cron_inner() {
+	PROCESS_NAME="$1"
+	NUMBER="$2"
+	echo "number=$NUMBER"
+	SUBCOMMAND=`echo $PROCESS_NAME|grep cronex`
+
+	echo ">$SUBCOMMAND"
+	if [[ ! -z $SUBCOMMAND ]]; then
+		recog_log "cronex" "antlabscronex.$NUMBER" &
+		PID="$!"
+		./crontest antlabscronex --count "$NUMBER" -d 10s &>/dev/null
+	else
+		recog_log "cron" "cron.$NUMBER" &
+		PID="$!"
+		./crontest robfigcron --count "$NUMBER" -d 10s &>/dev/null
+	fi
+	kill $PID
+}
+
+function recog_log() {
+	PROCESS_NAME="$1"
+	FILE_NAME="$2"
+	echo "process name:($PROCESS_NAME), file name:($FILE_NAME)"
+	while :;do
+		cpu=`ps aux |grep $PROCESS_NAME|grep -v grep|awk '{print $3}'`
+		echo "`date`, $cpu">> "$FILE_NAME.cpu.log"
+		mem=`ps aux |grep $PROCESS_NAME|grep -v grep|awk '{print $4}'`
+		echo "`date`, $mem">> "$FILE_NAME.mem.log"
+		sleep 1
+	done
+}
+
+function exit_handler() {
+	kill $PID
+	exit 0
+}
+
+echo "$1 $2"
+
+trap "exit_handler" SIGINT
+
+if [[ ! -z $1 ]];then
+	if [[ $1 == "10000" ]];then
+		cron_10000 $2
+	fi
+	exit 0
+fi
+
+rm *.log
+cron_10
